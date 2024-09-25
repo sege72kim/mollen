dbabd383c9bef6ffdbf74174f85c3c67003ba06e
sege72kim <sega72kim@mail.ru>
2024-09-25 21:15:02 +0500

    minor fix

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,11 +2,26 @@ import React from "react";
 import "./styles.css";
 
 const Card = ({ props }) => {
+  let color = "";
+  if (props.color === "Белый") {
+    color = "#f5f5f5";
+  } else if (props.color === "Серый") {
+    color = "#6b6868";
+  } else if (props.color === "Бежевый") {
+    color = "#edcda4";
+  } else if (props.color === "Синий") {
+    color = "#5d69c9";
+  } else if (props.color === "Красный") {
+    color = "#e85d5d";
+  }
   return (
     <div className="card">
       <div className="card_top">
         <p>{props.category}</p>
-        <img src="./icons/mollen.svg" alt="icon" />
+        {props.color === "special" && <img src="./icons/mollen.svg" alt="" />}
+        {props.color !== "special" && (
+          <div style={{ backgroundColor: color }} className="card_color" />
+        )}
       </div>
       <img className="card_img" src="./products/product1.webp" alt="product" />
       <h1>{props.name}</h1>
---
+++ src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+const ProductPage = (props) => {
+  return <div></div>;
+};
+
+export default ProductPage;
